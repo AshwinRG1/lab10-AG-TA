a1f58cd900121de64e84eff5720f6a7b5a1e2219
Ashwin <ashwin@Sivakotis-MacBook-Air.local>
2025-04-10 15:03:28 -0400

    merged

--- calculator.py
+++ calculator.py
@@ -30,11 +30,6 @@ def subtract(a, b):
 def mul(a,b):
     return a * b
 
-def div(a, b):
-    if b == 0:
-
-        raise ZeroDivisionError
-    return a / b
 
 def logarithm(a,b):
     if a <= 0 or b <= 0:
